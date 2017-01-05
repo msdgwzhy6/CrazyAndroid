7c7512b6085fb1e7d4c9459cce6e3151113d133b
CnPeng <893612134@qq.com>
2017-01-05 18:15:21 +0800

    启用数据绑定

--- app/src/main/java/com/cnpeng/cnpeng_demos2017_01/MainActivity.java
+++ app/src/main/java/com/cnpeng/cnpeng_demos2017_01/MainActivity.java
@@ -2,12 +2,19 @@ package com.cnpeng.cnpeng_demos2017_01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+    }
+
+    @Override
+    public void onClick(View v) {
+        
     }
 }
