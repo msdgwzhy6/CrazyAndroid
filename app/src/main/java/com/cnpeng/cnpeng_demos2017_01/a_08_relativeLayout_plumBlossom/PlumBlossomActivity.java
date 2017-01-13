1d1cf56aa6808ad20eb93c594c423d73db37e07e
CnPeng <893612134@qq.com>
2017-01-13 13:45:39 +0800

    07 使用帧布局实现霓虹灯效果

---
+++ app/src/main/java/com/cnpeng/cnpeng_demos2017_01/a_08_relativeLayout_plumBlossom/PlumBlossomActivity.java
@@ -0,0 +1,12 @@
+package com.cnpeng.cnpeng_demos2017_01.a_08_relativeLayout_plumBlossom;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by CnPeng on 2017/1/13.
+ * <p>
+ * 使用相对布局实现梅花效果
+ */
+
+public class PlumBlossomActivity extends AppCompatActivity {
+}
