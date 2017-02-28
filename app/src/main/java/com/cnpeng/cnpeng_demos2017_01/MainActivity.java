7fbb31425e0521b457a3d323121b756cb24062c6
CnPeng <893612134@qq.com>
2017-02-28 15:32:50 +0800

    37 CalendarView的基本使用示例

--- app/src/main/java/com/cnpeng/cnpeng_demos2017_01/MainActivity.java
+++ app/src/main/java/com/cnpeng/cnpeng_demos2017_01/MainActivity.java
@@ -42,6 +42,8 @@ import com.cnpeng.cnpeng_demos2017_01.a_33_ImageSwitcher.ImageSwitcherActivity;
 import com.cnpeng.cnpeng_demos2017_01.a_34_textSwitcher.TextSwitcherActvity;
 import com.cnpeng.cnpeng_demos2017_01.a_35_viewFlipper.ViewFlipperActviity;
 import com.cnpeng.cnpeng_demos2017_01.a_36_Toast.ImageToastActivity;
+import com.cnpeng.cnpeng_demos2017_01.a_37_1_CalendarPiackerDialog.CalendarPickerDialogActivity;
+import com.cnpeng.cnpeng_demos2017_01.a_37_calendarView.CalendarViewActivity;
 import com.cnpeng.cnpeng_demos2017_01.b_01_spannableString_ImageSpan.SpannableStringAndImageSpanActivity;
 import com.cnpeng.cnpeng_demos2017_01.databinding.ActivityMainBinding;
 import com.cnpeng.cnpeng_demos2017_01.utils.CommonUtils;
@@ -90,6 +92,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         binding.btTextSwitcher.setOnClickListener(this);
         binding.btViewFlipper.setOnClickListener(this);
         binding.btImageToast.setOnClickListener(this);
+        binding.btCalendarView.setOnClickListener(this);
+        binding.btCalendarPicker.setOnClickListener(this);
     }
 
     @Override
@@ -208,6 +212,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.bt_imageToast:
                 CommonUtils.mStartActivity(this, ImageToastActivity.class);
+                break;
+            case R.id.bt_calendarView:
+                CommonUtils.mStartActivity(this, CalendarViewActivity.class);
+                break;
+            case R.id.bt_calendarPicker:
+                CommonUtils.mStartActivity(this, CalendarPickerDialogActivity.class);
                 break;
         }
     }
---
+++ app/src/main/java/com/cnpeng/cnpeng_demos2017_01/a_37_calendarView/CalendarViewActivity.java
@@ -0,0 +1,36 @@
+package com.cnpeng.cnpeng_demos2017_01.a_37_calendarView;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+import com.cnpeng.cnpeng_demos2017_01.R;
+
+/**
+ * 作者：CnPeng
+ * <p>
+ * 时间：2017/2/28:上午10:56
+ * <p>
+ * 说明： CalendarView 日历视图的基本使用
+ */
+
+public class CalendarViewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendarview);
+
+        CalendarView calendarView = (CalendarView) findViewById(R.id.calendarView_01);
+
+        //设置日期改变时的监听器
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                //此处注意：返回的year 和 day 是准确的，但是 month是从0 开始计数的，所以在使用month时要手动+1。
+                Toast.makeText(CalendarViewActivity.this, "您的生日是" + year + (month + 1) + dayOfMonth, Toast
+                        .LENGTH_SHORT).show();
+            }
+        });
+    }
+}
